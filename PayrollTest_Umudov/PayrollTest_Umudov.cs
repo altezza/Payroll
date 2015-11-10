88b58804f08171c2f0e845a5c743d6b6d02a3f0a
altezza <bak23@yandex.ru>
2015-11-10 14:39:33 +0300

    10.11.15

---
+++ PayrollTest_Umudov/PayrollTest_Umudov.cs
@@ -0,0 +1,92 @@
+﻿using Payroll_Umudov;
+using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PayrollTest_Umudov
+{
+    [TestClass]
+    public class PayrollTest_Umudov
+    {
+        [TestMethod]
+        public void TestEmployee()
+        {
+            int empId = 1;
+            Employee e = new Employee(empId, "Bob", "Home");
+            Assert.AreEqual("Bob", e.Name);
+            Assert.AreEqual("Home", e.Address);
+            Assert.AreEqual(empId, e.EmpId);
+        }
+        [TestMethod]
+        public void TestEmployeeToString()
+        {
+            int empId = 1;
+            Employee e = new Employee(empId, "Bob", "Home");
+                        
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Emp#: ").Append(empId).Append("   ");
+            sb.Append("Bob").Append("   ");
+            sb.Append("Home").Append("   ");
+            sb.Append("Paid ").Append(e.Classification).Append("   ");
+            sb.Append(e.Schedule);
+            sb.Append(" by ").Append(e.Method);
+
+            Assert.AreEqual(e.ToString(), sb.ToString());
+        }
+
+        [TestMethod]
+        public void TestAddSalariedEmployee()
+        {
+            int empId = 1;
+            AddSalariedEmployee t = new AddSalariedEmployee(empId, "Bob", "Home", 1000.00);
+            t.Execute();
+            Employee e = PayrollDatabase.GetEmployee(empId);
+            Assert.AreEqual("Bob", e.Name);
+            PaymentClassification pc = e.Classification;
+            Assert.IsTrue(pc is SalariedClassification);
+            SalariedClassification sc = pc as SalariedClassification;
+            Assert.AreEqual(1000.00, sc.Salary, .001);
+            PaymentSchedule ps = e.Schedule;
+            Assert.IsTrue(ps is MonthlySchedule);
+            PaymentMethod pm = e.Method;
+            Assert.IsTrue(pm is HoldMethod);
+        }
+
+        [TestMethod]
+        public void TestAddHourlyEmployee()
+        {
+            int empId = 1;
+            AddHourlyEmployee t = new AddHourlyEmployee(empId, "Bob", "Home", 24);
+            t.Execute();
+            Employee e = PayrollDatabase.GetEmployee(empId);
+            Assert.AreEqual("Bob", e.Name);
+            PaymentClassification pc = e.Classification;
+            Assert.IsTrue(pc is HourlyClassification);
+            HourlyClassification sc = pc as HourlyClassification;
+            Assert.AreEqual(24, sc.HourlyRate, .001);
+            PaymentSchedule ps = e.Schedule;
+            Assert.IsTrue(ps is WeeklySchedule);
+            PaymentMethod pm = e.Method;
+            Assert.IsTrue(pm is HoldMethod);
+        }
+
+        [TestMethod]
+        public void TestAddCommissionedEmployee()
+        {
+            int empId = 1;
+            AddCommissionedEmployee t = new AddCommissionedEmployee(empId, "Bob", "Home", 1000.00, 0.50);
+            t.Execute();
+            Employee e = PayrollDatabase.GetEmployee(empId);
+            Assert.AreEqual("Bob", e.Name);
+            PaymentClassification pc = e.Classification;
+            Assert.IsTrue(pc is CommissionedClassification);
+            CommissionedClassification sc = pc as CommissionedClassification;
+            Assert.AreEqual(1000, sc.Salary, .001);
+            Assert.AreEqual(0.50, sc.CommissionRate, .001);
+            PaymentSchedule ps = e.Schedule;
+            Assert.IsTrue(ps is BiweeklySchedule);
+            PaymentMethod pm = e.Method;
+            Assert.IsTrue(pm is HoldMethod);
+        }
+    }
+}
---
+++ Payroll_Umudov/MonthlySchedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll_Umudov
+{
+    public class MonthlySchedule : PaymentSchedule
+    {
+        public override string ToString()
+        {
+            return "monthly";
+        }
+    }
+}
